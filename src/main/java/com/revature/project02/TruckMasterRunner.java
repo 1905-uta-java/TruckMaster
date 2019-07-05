128e371654eaa7190f631c1e85295ae549860db6
wolfe Magnus <wolfemagnus@nevada.unr.edu>
2019-07-05 15:10:42 -0500

    More structural alterations, spring boot.

---
+++ src/main/java/com/revature/project02/TruckMasterRunner.java
@@ -0,0 +1,13 @@
+package com.revature.project02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckMasterRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckMasterRunner.class, args);
+	}
+
+}
