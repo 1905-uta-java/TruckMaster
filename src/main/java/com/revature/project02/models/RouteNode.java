52c200b4b4abe1877a94504f1ebba72d13223804
Joshua Veit <joshuaveit1995@gmail.com>
2019-07-05 20:11:06 -0500

    adding slightly modified versions of the model files we put together on Thursday

---
+++ src/main/java/com/revature/project02/models/RouteNode.java
@@ -0,0 +1,74 @@
+package com.revature.project02.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties({"route"})
+public class RouteNode {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "routeNodeSequence")
+	@SequenceGenerator(name = "routeNodeSequence", sequenceName = "SQ_ROUTE_NODE_PK")
+	@Column(name = "ROUTE_NODE_ID")
+	private Integer id;
+	
+	@Column(name = "GEO_LOC")
+	private String location;
+	
+	@ManyToOne
+	@JoinColumn(name = "ROUTE_ID")
+	private Route route;
+	
+	@Column(name = "ORDER_POS")
+	private int order;
+	
+	public RouteNode() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public Route getRoute() {
+		return route;
+	}
+	
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+	
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+	@Override
+	public String toString() {
+		return "RouteNode [id=" + id + ", location=" + location + ", route=" + route + ", order=" + order + "]";
+	}
+}
---
+++ src/main/java/com/revature/project02/models/User.java
@@ -0,0 +1,74 @@
+package com.revature.project02.models;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "USERS")
+@JsonIgnoreProperties({"passHash"})
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userSequence")
+	@SequenceGenerator(name = "userSequence", sequenceName = "SQ_USER_PK")
+	@Column(name = "USER_ID")
+	private Integer id;
+	
+	@Column(name = "USERNAME", unique = true)
+	private String username;
+	
+	@Column(name = "PASS_HASH")
+	private String passHash;
+	
+	@Column(name = "EMAIL", unique = true)
+	private String email;
+	
+	@Column(name = "PHONE", unique = true)
+	private String phone;
+	
+	public User() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassHash() {
+		return passHash;
+	}
+	
+	public void setPassHash(String passHash) {
+		this.passHash = passHash;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
