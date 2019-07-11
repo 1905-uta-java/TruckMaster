8ae4c916489eea277e390b79df25b399ad23c812
wolfe Magnus <wolfemagnus@nevada.unr.edu>
2019-07-11 01:50:51 -0500

    Fixing UnauthorizedException's statuscode

--- src/main/java/com/revature/project02/controllers/ManagerController.java
+++ src/main/java/com/revature/project02/controllers/ManagerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,15 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.project02.exceptions.BadRequestException;
+import com.revature.project02.exceptions.UnauthorizedException;
 import com.revature.project02.models.Driver;
 import com.revature.project02.models.Manager;
 import com.revature.project02.models.Route;
+import com.revature.project02.models.UnencryptedAuthenticationToken;
 import com.revature.project02.services.DriverService;
 import com.revature.project02.services.ManagerService;
 import com.revature.project02.services.RouteService;
+import com.revature.project02.util.AuthTokenUtil;
 
 @CrossOrigin(origins = {"*"})
 @RestController
@@ -42,6 +46,7 @@ public class ManagerController {
 	 * Description - Creates a new Manager object and adds a new record of the new Manager into the db table
 	 * @param manager - json representation of the manager object
 	 * @param password - String representation of the manager's password
+	 * @param token - String representing the encrypted authentication token
 	 * @return - the newly create Manager object
 	 * @throws IOException 
 	 * @throws JsonMappingException 
@@ -49,13 +54,17 @@ public class ManagerController {
 	 * @throws - BadRequestException - if the manager parameter could not be instantiated. Usually by bad json.
 	 */
 	@PostMapping
-	public ResponseEntity<Manager> addManager(@RequestParam("manager") String managerStr, @RequestParam("password") String password) {
+	public ResponseEntity<Manager> addManager(@RequestParam("manager") String managerStr, @RequestParam("password") String password, @RequestHeader("token") String token) {
+		//Unencrypt the token
+		UnencryptedAuthenticationToken uat = AuthTokenUtil.fromEncryptedAuthenticationToken(token);
+		if(uat == null) throw new UnauthorizedException("Unauthorized Access!");
+		if(!"class com.revature.project02.models.Admin".equals(uat.getRole())) throw new UnauthorizedException("Unauthorized Access!");
+		
 		ObjectMapper om = new ObjectMapper();
 		Manager manager;
 		try {
 			manager = om.readValue(managerStr, Manager.class);
 		} catch (IOException e) {
-			e.printStackTrace();
 			throw new BadRequestException("invalid manager");
 		}
 		return new ResponseEntity<>(mService.addManager(manager, password), HttpStatus.CREATED);
@@ -65,11 +74,16 @@ public class ManagerController {
 	/**
 	 * Description - Returns a Manager of the provided driver id
 	 * @param id - Integer representation of the driver's id
+	 * @param token - String representing the encrypted authentication token 
 	 * @return - a Manager
 	 * @throws ResouceNotFoundException - If the manager does not exist or cannot be found.
 	 */
 	@GetMapping(value="/get-manager-driver-{id}")
-	public ResponseEntity<Manager> getDriverManager(@PathVariable("id") Integer id){
+	public ResponseEntity<Manager> getDriverManager(@PathVariable("id") Integer id, @RequestHeader("token") String token){
+		UnencryptedAuthenticationToken uat = AuthTokenUtil.fromEncryptedAuthenticationToken(token);
+		if(uat == null) throw new UnauthorizedException("Unauthorized Access!");
+		if(!"class com.revature.project02.models.Admin".equals(uat.getRole())) throw new UnauthorizedException("Unauthorized Access!");
+		
 		return new ResponseEntity<>(mService.getManagerByDriver(dService.getDriverById(id)), HttpStatus.OK);
 	}
 	
--- src/main/java/com/revature/project02/exceptions/UnauthorizedException.java
+++ src/main/java/com/revature/project02/exceptions/UnauthorizedException.java
@@ -1,5 +1,9 @@
 package com.revature.project02.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UnauthorizedException extends RuntimeException {
 	public UnauthorizedException(String msg) {
 		super(msg);
